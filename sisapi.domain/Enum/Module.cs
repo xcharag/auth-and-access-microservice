ab317910a5b952a8bdb4de6c4bf4b8e4333b1423
xcharag <oscaraguilarfut@gmail.com>
2026-03-14 02:24:48 -0400

    feat: Modify the enum to have NIBU as a module

--- sisapi.domain/Enum/Module.cs
+++ sisapi.domain/Enum/Module.cs
@@ -3,8 +3,5 @@ namespace sisapi.domain.Enum;
 public enum Module
 {
     SISAPI = 0, //Auth and Authorization module (API Gateway)
-    SIGA = 1, //Sistema Integrado de Gestión Administrativa (ERP Administrativo)
-    SISCON = 2, //Sistema Integrado de Contabilidad (ERP Contable)
-    SIBIR = 3, //Sistema Integrado de Bienes Raices (ERP B&R)
-    SIGC = 4 //Sistema Integrado de Gestion de Relaciones con el Cliente (CRM)
+    NIBU = 1 //Sistema NIBU
 }
