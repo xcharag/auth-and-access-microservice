9790430dbb1c9cf524ef14eee6ac15774966bfbd
xcharag <oscaraguilarfut@gmail.com>
2026-05-15 15:04:02 -0400

    feat: SportsApi module

--- sisapi.domain/Enum/Module.cs
+++ sisapi.domain/Enum/Module.cs
@@ -3,5 +3,6 @@ namespace sisapi.domain.Enum;
 public enum Module
 {
     SISAPI = 0, //Auth and Authorization module (API Gateway)
-    NIBU = 1 //Sistema NIBU
+    NIBU = 1, //Sistema NIBU
+    SportsApi = 2
 }
